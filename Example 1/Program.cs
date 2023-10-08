eb93467a80aeb1d9c521f2d04e6fbc5b8f4004ca
Yulia <y.chikaleva99@mail.ru>
2023-10-08 13:37:15 +0300

    Homework

---
+++ Example 1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 
+// 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using System.Xml.XPath;
+
+int Enter(string message) {
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int[] FillArray(int length) {
+    int[] array = new int[length];
+    for (int i=0; i<array.Length; i++) {
+        array[i] = Enter($"Введите {i+1}-й элемент массива");
+    }
+    return array;
+}
+
+void PrintArray(int[] array) {
+    for (int i=0; i<array.Length; i++) {
+        Console.Write(array[i]+ "\t");
+    }
+    Console.WriteLine();
+}
+
+int MoreZero(int[] array) {
+    int count = 0;
+    for (int i=0; i<array.Length;i++) {
+        if (array[i] > 0) {
+            count++;
+    }
+    }
+    return count;
+}
+
+int length = Enter("Введите количество элементов в массиве");
+int[] newArray = FillArray(length);
+PrintArray(newArray);
+Console.WriteLine($"Элементов в массиве больше 0 - {MoreZero(newArray)}");
+
